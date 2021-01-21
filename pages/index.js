f984790b6796286c072611e067d0e8c44ff9306f
Gustavo Souza <g.cflsouza@gmail.com>
2021-01-21 15:35:15 -0300

    links hero + full responsive version

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,15 @@ export default function Home() {
   <Container>
   
     <div className="content">
-      <h1><b>RR MakeUp</b> Catálogo 2021</h1>
-      <p>Para consultar a disponibilidade, clique no produto desejado. <br></br> Você será redirecionada para uma de nossas atendentes :)</p>
+      <h1>Catálogo <b>RR MakeUp</b></h1>
+      <p>Para consultar a disponibilidade, <b>clique em comprar</b>. Você será redirecionada para uma de nossas atendentes.</p>
+
+      <ul>
+        
+        <li><a href="#"><i class="fab fa-instagram"></i>Instagram </a></li>
+        <li><a href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a></li>
+        <li><a href="#"><i class="fas fa-map-marker-alt"></i> Como chegar</a></li>
+      </ul>
     </div>   
 
     </Container> 
@@ -52,7 +59,7 @@ export default function Home() {
 <Container>
 
 <Row>
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
      <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
@@ -63,9 +70,9 @@ export default function Home() {
   </Col>
 
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:eco,w_800/v1/imagens/products/Q20200572/kit-batom-vermelhissimo-e-esmalte-vermelherrimo.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$35</p>
 
@@ -74,9 +81,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://cdn.ultrafarma.com.br/static/produtos/812188/large-637054456057073563-812188_2.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$12</p>
 
@@ -85,9 +92,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://prebel.vteximg.com.br/arquivos/ids/160436-1000-1000/maquillaje-rostro-bases-catrice-base-liquida-hd-046-camel-beige-catrice-f9d7a0-pb0081186-sku_pb0081186_d7b691_1.png?v=637250119365430000" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -95,9 +102,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://cdn.awsli.com.br/600x450/987/987288/produto/36625679/pancake-branco-c973e3e0.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -105,9 +112,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://http2.mlstatic.com/D_NQ_NP_930269-MLB31100276046_062019-O.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -115,9 +122,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://www.luxbel.com.br/imagem/index/26295723/G/kit_omega_3_bell_corpus.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -125,9 +132,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://www.maxvalleshop.com.br/shop/product_images/j/014/higienizador_%28com_fundo%29_500x500__41804_std.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -135,9 +142,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://ae01.alicdn.com/kf/HTB1roIqPFXXXXcRXFXXq6xXFXXXW/229468445/HTB1roIqPFXXXXcRXFXXq6xXFXXXW.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
@@ -145,9 +152,9 @@ export default function Home() {
    </div>
   </Col>
 
-  <Col xs={6} md={3}>
+  <Col xs={6} md={4} xl={3}>
    <div className="product">
-     <img src="https://images.tcdn.com.br/img/img_prod/556508/base_hd_bege_02_liquida_yes_make_up_yes_cosmetics_749_1_20190116131304.png" />
+     <img src="https://dozlxztiddv53.cloudfront.net/Custom/Content/Products/10/13/1013430_fixador-de-maquiagem-ruby-rose_m1_636727984327085959.jpg" />
      <h3>Base HD Bege 02 Líquida Yes!</h3>
      <p className="price">R$40</p>
 
